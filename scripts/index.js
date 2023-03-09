dd607e18e97351eb23945df69b1f61a7c7be56b1
IlushinGL <ilushingl@yandex.ru>
2023-03-09 12:21:31 +0300

    feat: подготовить для ПР5

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,34 @@
+const initialCards = [
+  {
+    name: 'Москва 1964. Смоленская площадь',
+    link: '../images/el_1_ussr.jpg'
+  },
+  {
+    name: 'Нью-Йорк. Вид изнутри',
+    link: '../images/el_2_usa.jpg'
+  },
+  {
+    name: 'Париж. Башня Эйфеля',
+    link: '../images/el_3_france.jpg'
+  },
+  {
+    name: 'Где-то в горах',
+    link: '../images/el_4_mountains.jpg'
+  },
+  {
+    name: 'Сидней',
+    link: '../images/el_5_australia.jpg'
+  },
+  {
+    name: 'Флоренция',
+    link: '../images/el_6_italy.jpg'
+  }
+];
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log(initialCards);
+})
+
 let pgAuth = document.querySelector('#pgAuth');
 let pgInfo = document.querySelector('#pgInfo');
 let pageProfileEditButton = document.querySelector('.profile__edit-button');
