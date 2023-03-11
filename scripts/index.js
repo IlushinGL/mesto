703a295c875ce7705ae036f683dbf5bee3265bd4
IlushinGL <ilushingl@yandex.ru>
2023-03-11 21:21:27 +0300

    fix: проверить ссылки

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const elementsInBox = [
   {
     name: 'Москва 1964. Смоленская площадь',
-    link: '../images/el_1_ussr.jpg'
+    link: './images/el_1_ussr.jpg'
   },
   {
     name: 'Нью-Йорк. Вид изнутри',
-    link: '../images/el_2_usa.jpg'
+    link: './images/el_2_usa.jpg'
   },
   {
     name: 'Париж. Башня Эйфеля',
-    link: '../images/el_3_france.jpg'
+    link: './images/el_3_france.jpg'
   },
   {
     name: 'Где-то в горах',
-    link: '../images/el_4_mountains.jpg'
+    link: './images/el_4_mountains.jpg'
   },
   {
     name: 'Сидней',
-    link: '../images/el_5_australia.jpg'
+    link: './images/el_5_australia.jpg'
   },
   {
     name: 'Флоренция',
-    link: '../images/el_6_italy.jpg'
+    link: './images/el_6_italy.jpg'
   }
 ];
 
