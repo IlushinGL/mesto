de5499fe44e3eb42342973045f89445a7119ecad
IlushinGL <ilushingl@yandex.ru>
2023-04-19 11:10:27 +0300

    fix: воостановить импорт по умолчанию

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,4 +1,4 @@
-export class Card {
+export default class Card {
   constructor(templateSelector, imgClickFunction) {
     this._templateSelector = templateSelector;
     this._imgClickFunction = imgClickFunction;
